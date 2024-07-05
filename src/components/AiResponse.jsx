6b762484c61d82f347dad68bb797bdef040dd45a
jackop <lolits2626@gmail.com>
2024-07-06 01:15:39 +0530

    auth0 key set to .env

--- src/components/AiResponse.jsx
+++ src/components/AiResponse.jsx
@@ -2,9 +2,6 @@ import React,{useEffect} from 'react'
 import Prism from 'prismjs';
 import Markdown from 'markdown-to-jsx';
 import "./codetheme/prism-darcula.css"
-// import 'prismjs/components/prism-core';
-// import 'prismjs/components/prism-clike';
-// import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-python';
 // import 'prismjs/components/prism-cpp';
--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom"
-import ChatAi from "./components/ChatAi"
-import ErrorPage from "./components/ErrorPage"
-import { Auth0Provider } from "@auth0/auth0-react"
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import ChatAi from "./components/ChatAi";
+import ErrorPage from "./components/ErrorPage";
+import { Auth0Provider } from "@auth0/auth0-react";
 const router = createBrowserRouter([
-            {
-                path:"/",
-                element:<App/>
-            },
-            {
-               path:"/chatai" ,
-               element:<ChatAi/>
-            },
-            {
-                path:"/*",
-                element:<ErrorPage/>
-            }
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/chatai",
+    element: <ChatAi />,
+  },
+  {
+    path: "/*",
+    element: <ErrorPage />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Auth0Provider
-    domain="dev-d7ofl5e76ija7qcf.us.auth0.com"
-    clientId="0EvDad3XwwELrVSp9oGpQZxgY74C6m9t"
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENTID}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
     }}
   >
-    <RouterProvider router={router}/>
-  </Auth0Provider>,
-)
+    <RouterProvider router={router} />
+  </Auth0Provider>
+);
